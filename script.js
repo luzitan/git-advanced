b0ac5a339ae405b2ebe36fb4b723bbe0ed50c662
luzitan <160033831+luzitan@users.noreply.github.com>
2024-05-05 16:01:00 +0300

    remoted alert

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
     document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
-            alert('Длина текста: ' + text.length);
             // some comment
         });
     });
